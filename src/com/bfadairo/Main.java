1e89722a8ef21441606fb45958bc0a032129508d
BFadairo <BrandonFadairo@hotmail.com>
2021-12-01 12:08:46 -0500

    Added Solutions for Day One: Sonar Sweep

--- src/com/bfadairo/Main.java
+++ src/com/bfadairo/Main.java
@@ -1,8 +1,11 @@
 package com.bfadairo;
 
+import com.bfadairo.y2021.SonarSweep;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        SonarSweep.Sweep("src/com/bfadairo/y2021/inputs/day-1.txt");
     }
 }
--- src/com/bfadairo/y2021/SonarSweep.java
+++ src/com/bfadairo/y2021/SonarSweep.java
@@ -6,58 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- * --- Day 1: Sonar Sweep ---
- *
- * You're minding your own business on a ship at sea when the overboard alarm goes off! You rush to see if you can help. Apparently, one of the Elves tripped and accidentally sent the sleigh keys flying into the ocean!
- *
- * Before you know it, you're inside a submarine the Elves keep ready for situations like this. It's covered in Christmas lights (because of course it is), and it even has an experimental antenna that should be able to track the keys if you can boost its signal strength high enough; there's a little meter that indicates the antenna's signal strength by displaying 0-50 stars.
- *
- * Your instincts tell you that in order to save Christmas, you'll need to get all fifty stars by December 25th.
- *
- * Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
- *
- * As the submarine drops below the surface of the ocean, it automatically performs a sonar sweep of the nearby sea floor. On a small screen, the sonar sweep report (your puzzle input) appears: each line is a measurement of the sea floor depth as the sweep looks further and further away from the submarine.
- *
- * For example, suppose you had the following report:
- *
- * 199
- * 200
- * 208
- * 210
- * 200
- * 207
- * 240
- * 269
- * 260
- * 263
- *
- * This report indicates that, scanning outward from the submarine, the sonar sweep found depths of 199, 200, 208, 210, and so on.
- *
- * The first order of business is to figure out how quickly the depth increases, just so you know what you're dealing with - you never know if the keys will get carried into deeper water by an ocean current or a fish or something.
- *
- * To do this, count the number of times a depth measurement increases from the previous measurement. (There is no measurement before the first measurement.) In the example above, the changes are as follows:
- *
- * 199 (N/A - no previous measurement)
- * 200 (increased)
- * 208 (increased)
- * 210 (increased)
- * 200 (decreased)
- * 207 (increased)
- * 240 (increased)
- * 269 (increased)
- * 260 (decreased)
- * 263 (increased)
- *
- * In this example, there are 7 measurements that are larger than the previous measurement.
- *
- * How many measurements are larger than the previous measurement?
- */
 public class SonarSweep {
 
-    public static int Sweep(String filePath) {
-        List<Integer> sweeps = readFile(filePath);
-        return countPositiveChanges(sweeps);
+    public static void Sweep(String filePath) {
+        List<Integer> depths = readFile(filePath);
+        int partOneAns = partOneCountDepthIncreases(depths);
+        int partTwoAns = partTwoCountThreeSumDepthIncreases(depths);
+        System.out.println("Part One Ans = " + partOneAns);
+        System.out.println("Part Two Ans = " + partTwoAns);
     }
 
     /**
@@ -84,14 +40,40 @@ public class SonarSweep {
     /**
      * Iterates through the Sweeps List and returns the amount of times the next value is greater than the previous
      * O(N) Runtime
-     * @param sweeps - A List of Sweeps
+     * @param depths - A List of Sweeps
      * @return - Amount of times next value was greater than previous
      */
-    private static int countPositiveChanges(List<Integer> sweeps) {
+    private static int partOneCountDepthIncreases(List<Integer> depths) {
         int count = 0;
 
-        for (int i = 1; i < sweeps.size(); i++) {
-            if (sweeps.get(i) > sweeps.get(i - 1)) count++;
+        for (int i = 1; i < depths.size(); i++) {
+            if (depths.get(i) > depths.get(i - 1)) count++;
+        }
+
+        return count;
+    }
+
+    /**
+     * Iterates through the Sweeps list and returns the amount of times sum of the next three values
+     * is greater than the previous sum
+     * @param depths - List of Depth Measurements from Sonar Sweeps
+     * @return - # of times depth increases from previous
+     */
+    private static int partTwoCountThreeSumDepthIncreases(List<Integer> depths) {
+        int previousSum = Integer.MAX_VALUE;
+        int count = 0;
+
+        for (int i = 2; i < depths.size(); i++) {
+            int valOne = depths.get(i - 2);
+            int valTwo = depths.get(i - 1);
+            int valThree = depths.get(i);
+            int sum = valOne + valTwo + valThree;
+
+            if (sum > previousSum) {
+                count++;
+            }
+
+            previousSum = sum;
         }
 
         return count;
