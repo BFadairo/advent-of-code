70f6f56cf7869b69a01a1830701c2d8898c98107
BFadairo <BrandonFadairo@hotmail.com>
2021-12-24 03:38:20 -0500

    Update Main class

--- src/com/bfadairo/Main.java
+++ src/com/bfadairo/Main.java
@@ -1,13 +1,15 @@
 package com.bfadairo;
 
-import com.bfadairo.y2021.Dive;
-import com.bfadairo.y2021.SonarSweep;
+import com.bfadairo.y2021.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 //        SonarSweep.Sweep("src/com/bfadairo/y2021/inputs/day-1.txt");
-        Dive.dive("src/com/bfadairo/y2021/inputs/day-2.txt");
+//        Dive.dive("src/com/bfadairo/y2021/inputs/day-2.txt");
+//        BinaryDiagnostic.diagnose("src/com/bfadairo/y2021/inputs/day-3.txt");
+//        GiantSquid.bingo("src/com/bfadairo/y2021/inputs/day-4.txt");
+        HydrothermalVenture.venture("src/com/bfadairo/y2021/inputs/day-5.txt");
     }
 }
